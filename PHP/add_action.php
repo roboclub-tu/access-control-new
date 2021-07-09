b98814f64f776629ebb0b2864efda40fd4406b82
BuildTools <dani.matev123@gmail.com>
2021-07-09 12:26:12 +0300

    changed all sql statements to be predefined for better maintainability

--- PHP/add_action.php
+++ PHP/add_action.php
@@ -11,11 +11,19 @@
     define('API_KEY', 'r2543fgo2');
 
     //variables posted from ESP, sync with ESP
+    //TODO check if you can get them from a .h file to automatically sync with ESP
     define('ADD_TAG', 'ADD_TAG');
     define('DEL_TAG', 'DEL_TAG');
     define('LOCK', 'LOCK');
     define('UNLOCK', 'UNLOCK');
     define('SCAN', 'SCAN');
+
+    //SQL statements used for different actions
+    define('ADD_TAG_QUERY', 'CALL tag_added_esp(\"$tag_hex\")');
+    define('DEL_TAG_QUERY', 'CALL tag_deleted_esp(\"$tag_hex\")');
+    define('LOCK_QUERY', 'INSERT INTO actions_esp(tag_hex, action) VALUES(\"$tag_hex\", \"lock\")');
+    define('UNLOCK_QUERY', 'INSERT INTO actions_esp(tag_hex, action) VALUES(\"$tag_hex\", \"unlock\")');
+    define('SCAN_QUERY', 'INSERT INTO actions_esp(tag_hex, action) VALUES(\"$tag_hex\", \"scan\")');
 
     $mysql = new mysqli(DB_HOST, DB_USENAME, DB_PASSWORD, DB_NAME);
 
@@ -34,13 +42,20 @@
 
         switch($action) {
             case ADD_TAG:
-                $query = "CALL tag_added_esp(\"$tag_hex\")";
+                $query = ADD_TAG_QUERY;
                 break;
             case DEL_TAG:
-                $query = "CALL tag_deleted_esp(\"$tag_hex\")";
+                $query = DEL_TAG_QUERY;
                 break;
             case LOCK:
-                $query = "INSERT INTO actions_esp(tag_hex, action) VALUES(\"$tag_hex\", \"LOCK\")";
+                $query = LOCK_QUERY;
+                break;
+            case UNLOCK:
+                $query = UNLOCK_QUERY;
+                break;
+            case SCAN:
+                $query = SCAN_QUERY;
+                break;
         }
 
         if($mysql->query($query)) {
